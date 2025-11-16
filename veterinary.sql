43680fadc0b308382fe1ed958bf7dbc65ca3b22e
rhyancornejosanchez-web <rhyancornejosanchez@gmail.com>
2025-11-17 00:28:15 +0800

    feat/create-table-appointments

--- veterinary.sql
+++ veterinary.sql
@@ -22,6 +22,13 @@ CREATE TABLE animals (
     FOREIGN KEY (ownerid) REFERENCES owners(ownerid)
 );
 
+CREATE TABLE appointments (
+    appointid INT PRIMARY KEY,
+    animalid INT,
+    appointdate DATE,
+    reason VARCHAR(255),
+    FOREIGN KEY (animalid) REFERENCES animals(animalid)
+);
 
 
 
